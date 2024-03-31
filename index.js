41e6519d03edfabaa41bafef881bd6435b2d548a
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-04-01 02:08:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers,query){
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+
+}
+
+// find the names that begin with the given letters
+function fuzzyMatch (drivers,query){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+
+
+}
+// find matching names with the given strings
+function matchName(drivers, query){
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+    
+}
